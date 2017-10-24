9884ea678ce918a3e89bda1ef3f58a81714d4ba3
Dillon Christensen <j.dillon.christensen@gmail.com>
2017-10-24 16:41:06 -0500

    prepared for release and usage, posts to twitter now @cubedeskface

--- index.js
+++ index.js
@@ -1,55 +1,115 @@
 require('dotenv').config();
-const fs = require('fs');
 const nw = require('node-webcam');
-const request = require('request');
-const moment = require('moment');
-const API_URL = 'https://slack.com/api/files.upload';
-const API_TOKEN = process.env.SLACK_API_TOKEN;
-const nwOptions = {
-    delay: 0,
-    width: 1280,
-    height: 720,
-    quality: 80,
-    saveShots: false,
-    output: 'jpeg',
-    callbackReturn: 'location'
-};
+const {exec} = require('child_process');
+const fs = require('fs');
+const snapshotName = 'test-snapshot';
+const snapshotPath = snapshotName + '.jpg';
+const lastPhotoPath = 'last-snapshot.jpg';
+const Twitter = require('twitter');
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
+function takePicture() {
+    return new Promise((resolve, reject) => 
+        nw.capture(snapshotName, {
+            delay: 0,
+            width: 1280,
+            height: 720,
+            quality: 80,
+            saveShots: false,
+            output: 'bmp',
+            callbackReturn: 'location'
+        }, (err, data) => {
+            if (!err && data) {
+                resolve(data);
+            } else {
+                reject(err);
+            }
+        })
+    )
+    .then(convertSnapshot);
+}
+
+function convertSnapshot() {
+    return new Promise((res, rej) => {
+        exec(`magick convert ${snapshotName}.bmp ${snapshotPath}`, (err, out, serr) => {
+            if (err) {
+                console.error('could not convert to jpg', err);
+            }
+            res();
+        });
+    });
+}
+
+function diff() {
+    return new Promise((res, rej) => exec(`magick compare -metric RMSE ${snapshotPath} ${lastPhotoPath} diff.jpg`, (err, out, serr) => {
+        let isDiff = true;
+        try {
+            const diff = +serr.match(/\(([\d.]+)\)/)[1];
+            //console.log('PicDiff', diff);
+            isDiff = diff > 0.08;
+        } catch (ignore) {}
+        res(isDiff);
+    }));
+}
+
+function deleteLastPhoto() {
+    return new Promise((res, rej) => fs.unlink(lastPhotoPath, err => {
+        res();
+    }));
+}
+
+function moveSnapshotToLastPhoto() {
+    return new Promise((res, rej) => fs.rename(snapshotPath, lastPhotoPath, err => {
+        res();
+    }));
+}
+
+function postPhoto() {
+    return new Promise((res, rej) => {
+        fs.readFile(snapshotPath, (err, media) => {
+            if (err || !media) {
+                console.error(err || 'no data...');
+            } else {
+                client.post('media/upload', {media}, (err, med, resp) => {
+                    if (!err) {
+                        const status = {
+                            status: '',
+                            media_ids: med.media_id_string
+                        };
+                        client.post('statuses/update', status, (err, t, resp) => {
+                            if (!err) {
+                                //    console.log('YES INDEEDY', t);
+                            } else {
+                                console.error('tweet error', err);
+                            }
+                        });
+                    } else {
+                        console.error('Twitter post error', err);
+                    }
+                    res();
+                });
+            }
+        });
+    });
+}
 
 function takeAndPostPic() {
-    new Promise((resolve, reject) => nw.capture('snapshot', nwOptions, (err, data) => {
-        if (!err && data) {
-            resolve(data);
-        } else {
-            reject(err);
-        }
-    }))
-        .then(location => request.post({
-                url: API_URL,
-                formData: {
-                    token: API_TOKEN,
-                    file: fs.createReadStream(location),
-                    filename: 'snapshot.jpg',
-                    title: `mydesk--${moment().format('MM-DD-Y--hh-mm-ss')}`,
-                    channels: 'deskface'
-                }
-            }, (err, response, body) => {
-                if (!err) {
-                    try {
-                        const result = JSON.parse(body);
-                        if (!result.ok) {
-                            console.error(body);
-                        } else {
-                            console.log('YES INDEEDY');
-                            goRandomly();
-                        }
-                    } catch (errj) {
-                        console.error('JSON ERROR', errj);
-                    }
-                } else {
-                    console.error(err);
-                }
-            }));
+    takePicture()
+        .then(diff)
+        .then(isDiff => {
+            if (isDiff) {
+                //console.log(`POSTING ${snapshotName}!`);
+                return postPhoto();
+            }
+            //console.log(`NOT POSTING ${snapshotName}`);
+        })
+        .then(deleteLastPhoto)
+        .then(moveSnapshotToLastPhoto);
 }
 
 function goRandomly() {
--- test.js
+++ test.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const nw = require('node-webcam');
 const {exec} = require('child_process');
 const fs = require('fs');
 const snapshotName = 'test-snapshot';
-const lastPhotoName = 'last-snapshot.jpg';
+const snapshotPath = snapshotName + '.jpg';
+const lastPhotoPath = 'last-snapshot.jpg';
+const Twitter = require('twitter');
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 function takePicture() {
     return new Promise((resolve, reject) => 
@@ -12,7 +21,7 @@ function takePicture() {
             height: 720,
             quality: 80,
             saveShots: false,
-            output: 'jpeg',
+            output: 'bmp',
             callbackReturn: 'location'
         }, (err, data) => {
             if (!err && data) {
@@ -21,16 +30,27 @@ function takePicture() {
                 reject(err);
             }
         })
-    );
+    )
+    .then(convertSnapshot);
+}
+
+function convertSnapshot() {
+    return new Promise((res, rej) => {
+        exec(`magick convert ${snapshotName}.bmp ${snapshotPath}`, (err, out, serr) => {
+            if (err) {
+                console.error('could not convert to jpg', err);
+            }
+            res();
+        });
+    });
 }
 
 function diff() {
-    return new Promise((res, rej) => exec(`magick compare -metric RMSE ${snapshotName}.jpg ${lastPhotoName} diff.jpg`, (err, out, serr) => {
+    return new Promise((res, rej) => exec(`magick compare -metric RMSE ${snapshotPath} ${lastPhotoPath} diff.jpg`, (err, out, serr) => {
         let isDiff = true;
         try {
-            console.log(serr);
             const diff = +serr.match(/\(([\d.]+)\)/)[1];
-            console.log('PicDiff', diff);
+            //console.log('PicDiff', diff);
             isDiff = diff > 0.08;
         } catch (ignore) {}
         res(isDiff);
@@ -38,25 +58,62 @@ function diff() {
 }
 
 function deleteLastPhoto() {
-    return new Promise((res, rej) => fs.unlink(lastPhotoName, err => {
+    return new Promise((res, rej) => fs.unlink(lastPhotoPath, err => {
         res();
     }));
 }
 
 function moveSnapshotToLastPhoto() {
-    return new Promise((res, rej) => fs.rename(snapshotName + '.jpg', lastPhotoName, err => {
+    return new Promise((res, rej) => fs.rename(snapshotPath, lastPhotoPath, err => {
         res();
     }));
 }
 
-takePicture()
-    .then(diff)
-    .then(isDiff => {
-        if (isDiff) {
-            console.log(`POSTING ${snapshotName}!`);
-        } else {
-            console.log(`NOT POSTING ${snapshotName}`);
-        }
-    })
-    .then(deleteLastPhoto)
-    .then(moveSnapshotToLastPhoto);
+function postPhoto() {
+    return new Promise((res, rej) => {
+        fs.readFile(snapshotPath, (err, media) => {
+            if (err || !media) {
+                console.error(err || 'no data...');
+            } else {
+                client.post('media/upload', {media}, (err, med, resp) => {
+                    if (!err) {
+                        const status = {
+                            status: '',
+                            media_ids: med.media_id_string
+                        };
+                        client.post('statuses/update', status, (err, t, resp) => {
+                            if (!err) {
+                                //    console.log('YES INDEEDY', t);
+                            } else {
+                                console.error('tweet error', err);
+                            }
+                        });
+                    } else {
+                        console.error('Twitter post error', err);
+                    }
+                    res();
+                });
+            }
+        });
+    });
+}
+
+function takeAndPostPic() {
+    takePicture()
+        .then(diff)
+        .then(isDiff => {
+            if (isDiff) {
+                //console.log(`POSTING ${snapshotName}!`);
+                return postPhoto();
+            }
+            //console.log(`NOT POSTING ${snapshotName}`);
+        })
+        .then(deleteLastPhoto)
+        .then(moveSnapshotToLastPhoto);
+}
+
+function goRandomly() {
+    setTimeout(takeAndPostPic, (~~(Math.random() * 20) + 5) * 60000);
+}
+
+goRandomly();
